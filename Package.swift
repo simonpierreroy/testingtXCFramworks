04231b775f27d67882c533f0db12cc4f4119cfa5
Simon-Pierre Roy <sroy1@wayfair.com>
2020-10-26 13:06:29 -0400

    bla

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FirebaseCrashlytics",
-            url: "https://github.com/simonpierreroy/testingtXCFramworks/releases/download/v6.23/FirebaseCrashlytics-6.23.xcframework.zip",
+            url: "https://github.com/simonpierreroy/testingtXCFramworks/releases/download/6.23/FirebaseCrashlytics-6.23.xcframework.zip",
             checksum: "808047a82c339d1c3d903ae128788057f8bb52c36016e0f8e5978ecdaed5fce0"
         )
         
